b70fa5b855d40d3c81be8554a0faa762b33b26e4
Fernando Garcia Borges <fgborges@protonmail.com>
2019-08-01 21:26:20 +0900

    Upgrade dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
-import "latex-elements/dist/latex-elements.min.js";
-import "code-elements/dist/code-elements.min.js";
+import "latex-elements";
+import "code-elements";
 import "bootswatch/dist/%THEME%/bootstrap.css";
 render(<App />, document.getElementById("root"));
